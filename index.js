1f2aa3bf5fda47e94bf1abd1dae2557cacd4f69c
GresaKrasniqi <gresak@metropolia.fi>
2023-03-13 15:10:02 +0200

    server-one done

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+'use strict';
 
-console.log("hello world");
-const _ = require(lodash);
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
 
-const output = _.camelCase(foo bar);
-console.log(output);
+app.get("/catinfo", (req, res) => {
+    const cat = {
+      name: "Frank",
+      birthdate: "2010-12-25",
+      weight: 5,
+    };
+    res.json(cat);
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
