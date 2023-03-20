e4588aa650388844d2069f8be759f1c521d65dc4
GresaKrasniqi <gresak@metropolia.fi>
2023-03-20 12:38:49 +0200

    week2 part2

--- controllers/catController.js
+++ controllers/catController.js
@@ -1,2 +1,37 @@
 'use strict';
-// catController
+const catModel = require('../models/catModel');
+
+const cats = catModel.cats;
+
+const getCatList = (req, res) => {
+    res.json(cats);
+};
+
+const getCat = (req, res) => {
+    //console.log(req.params);
+    const id = req.params.catId;
+    //todo filter matching cat based on id
+    //todo response 404 of od not found in array (res.status(404))
+    const cat = cats [id-1];
+    if (catModel.length) {
+        res.json(catModel);
+      } else {
+        res.status(404).send("no data");
+      }
+    res.json();
+};
+
+const postCat = (req, res) => {
+    res.send('With this endpoint you can add cats.');
+};
+
+const putCat = (req, res) => {
+    res.send('From this endpoint you can modify cats.');
+};
+
+const deleteCat = (req, res) => {
+    res.send('From this endpoint you can delete a cat.');
+};
+
+const catController = (getCatList, getCat, postCat, putCat, deleteCat);
+module.exports = catController;
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,2 +1,14 @@
 'use strict';
-// catRoute
+const express = require('express');
+const router = express.Router();
+const catController = require('../controllers/catController');
+
+router.get('/', catController.getCatList);
+router.get('/:catId', catController.getCat);
+router.post('/', catController,postCat);
+router.put('/', catController, putCat);
+router.delete('/', catController, deleteCat);
+
+module.exports = router;
+
+
